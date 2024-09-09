910934d2ec0919e8bbf213eaf306159d0e456a87
McKenna H <mckennah@McKennas-Laptop.local>
2024-09-08 20:23:47 -0600

    homework push

---
+++ 2.8/googlePage.ts
@@ -0,0 +1,19 @@
+import { By } from "selenium-webdriver";
+import { BasePage } from './basePage';
+
+export class Google extends BasePage {
+    searcBar: By = By.name('q');
+    results: By = By.id('rcnt');
+
+    constructor() {
+        super ({url:"https://www.google.com/" });
+    };
+    async search(thingToSearch: string) {
+        return this.setInput(this.searchBar, `${thingToSearch}`);
+    };
+    async getResults() {
+        return this.getText(this.results);
+        
+
+    }
+}
